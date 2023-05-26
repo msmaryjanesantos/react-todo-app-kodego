7703db38ffe00d9e43991cdcc08c71eefe752a73
Mary Jane Santos <msmaryjanesantos@gmail.com>
2023-05-26 22:19:59 +0800

    my first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import "./NavbarStyles.css";
+
+import React from 'react'
+import {link } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+   <div className="header">
+    <Link to="/"></Link>
+  </div>
+  )
+};
+
+export default Navbar;
